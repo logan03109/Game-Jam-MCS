9cc7066bb553a19cc6e3bea5f2d40904064c374e
GDLT <fc4m57mdcv@privaterelay.appleid.com>
2026-06-02 14:49:26 +0100

    Fixed double jump code

--- scripts/player_1.gd
+++ scripts/player_1.gd
@@ -13,12 +13,6 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-<<<<<<< HEAD:scripts/player_1.gd
-<<<<<<< Updated upstream:scripts/cube.gd
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-=======
 	if is_on_floor():
 		jump_count = 2
 	
@@ -26,17 +20,9 @@ func _physics_process(delta):
 		jump_count -= 1 
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis(left_action, right_action)
->>>>>>> Stashed changes:scripts/player_1.gd
-=======
-	if Input.is_action_just_pressed(jump_action) and jump_count != 1:
-		velocity.y = JUMP_VELOCITY
-		jump_count -= 1 
-	if is_on_floor():
-		jump_count = 2
 
 	var direction := Input.get_axis(left_action, right_action)
->>>>>>> main:scripts/cube.gd
+
 
 	if direction:
 		velocity.x = direction * SPEED
