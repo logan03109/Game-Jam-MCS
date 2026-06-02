a4b6e11b1af162f892e00dc60808db890c83be94
logan03109 <wonglogan69@gmail.com>
2026-06-02 14:15:42 +0100

    double jump

--- scripts/cube.gd
+++ scripts/cube.gd
@@ -5,14 +5,19 @@ extends CharacterBody2D
 @export var jump_action := "p1_jump"
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -800.0
+
+var jump_count = 2
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed(jump_action) and is_on_floor():
+	if Input.is_action_just_pressed(jump_action) and jump_count != 1:
 		velocity.y = JUMP_VELOCITY
+		jump_count -= 1 
+	if is_on_floor():
+		jump_count = 2
 
 	var direction := Input.get_axis(left_action, right_action)
 
