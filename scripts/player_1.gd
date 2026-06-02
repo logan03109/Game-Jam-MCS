811d760cdd68bf555ff6689713c6fffe177a37db
GDLT <fc4m57mdcv@privaterelay.appleid.com>
2026-06-02 14:30:54 +0100

    Fixed double jump code

--- scripts/player_1.gd
+++ scripts/player_1.gd
@@ -10,9 +10,20 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+<<<<<<< Updated upstream:scripts/cube.gd
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+=======
+	if is_on_floor():
+		jump_count = 2
+	
+	if Input.is_action_just_pressed(jump_action) and jump_count > 0:
+		jump_count -= 1 
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis(left_action, right_action)
+>>>>>>> Stashed changes:scripts/player_1.gd
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
